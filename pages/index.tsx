a4a2102fc8ece0bb8ecdcedfa38578b6ea184014
Camilo Gonzalez <jmilo.dev@gmail.com>
2022-01-15 20:07:16 -0300

    change styles of heroImage and tesxt description of home

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,21 +9,21 @@ const welcome = '/images/welcome.png'
 export default function Home(){
     return(
         <article className={styles.home}>
-            <section>
-                <HeroHome 
-                    src={welcome} 
-                    text='Saludo' 
-                    width={142} 
-                    height={142} 
-                    title='Camilo Gonzalez'
-                    subtitle='Frontend Developer'
-                />
+            <HeroHome 
+                src={welcome} 
+                text='Saludo' 
+                width={142} 
+                height={142} 
+                title='Camilo Gonzalez'
+                subtitle='Frontend Developer'
+            />
+            <section className={styles.description}>
                 <p>
                     ¡Hola! 
                     Te doy la bienvenida a mi web, la mejor forma de mostrarte mis habilidades y contarte un poco sobre mi.
                 </p>
             </section>
-            <section className={styles.home_sectionButtons}>
+            <section className={styles.sectionButtons}>
                 <SiteSection link="/sobre-mi" text="Sobre mí"/>
                 <SiteSection link="/tecnologias" text="Tecnologías"/>
                 <SiteSection link="/proyectos" text="Proyectos"/>
