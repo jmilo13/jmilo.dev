d85c3e8132ec1804f4d0c1385835b766b39a95bf
Camilo Gonzalez <jmilo.dev@gmail.com>
2022-01-19 20:13:46 -0300

    fix error in modal of aditional information

--- pages/proyectos.tsx
+++ pages/proyectos.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, {useState} from 'react'
 import SectionTitle from '@components/SectionTitle'
 import { projects } from '@utils/lists'
 import styles from '@styles/projects.module.scss'
 
 
 export default function Projects(){
+  const[distance, setDistance] = useState(0)
     return(
       <>
         <SectionTitle title='Proyectos'/>
@@ -12,9 +13,9 @@ export default function Projects(){
           {projects.map(project => {
             const changeVisibility = (e:any) => {
               const element = document.querySelector(`#${project.id}`)
+              setDistance(window.scrollY)
               if(e.target.id === 'open'){
                 element?.classList.add(styles.show)
-                window.scrollTo(0,0)
               }else{
                 element?.classList.remove(styles.show)
               }
@@ -32,7 +33,7 @@ export default function Projects(){
               <a href={project.repoUrl} target='_blank'>Ver repositorio</a>
               <div>
                 <span id='open' onClick={changeVisibility}>+ Información</span>
-                <div id={`${project.id}`} className={styles.tooltip}>
+                <div id={`${project.id}`} className={styles.tooltip} style={{top: `${distance}px`}}>
                   <div className={styles.content}>
                     <button id='close' className={styles.close} onClick={changeVisibility}></button>
                     {project.aditionalData.map(data => <p key={`data${project.aditionalData.indexOf(data)}`}>{data}</p>)}
