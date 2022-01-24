62f18c8160efa4239acbc6a0aed58ac1eb56030b
Camilo Gonzalez <jmilo.dev@gmail.com>
2022-01-24 12:00:38 -0300

    include itemProp in meta to show image in whatsapp link

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,7 +10,7 @@ class MyDocument extends Document {
     return (
       <Html lang='es'>
         <Head>
-          <meta property="og:image" content='https://jmilo-dev.vercel.app/images/profile.png' key='ogimage'/>
+          <meta property="og:image" itemProp='image' content='https://jmilo-dev.vercel.app/images/profile.png' key='ogimage'/>
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
